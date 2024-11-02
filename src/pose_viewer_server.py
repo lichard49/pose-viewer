0f3e4b539b084ee8286fac0679b1536602db48be
Richard Li <lichard49@gmail.com>
2024-11-01 22:15:04 -0700

    Laid out HTML and Javascript directory structure

--- src/pose_viewer_server.py
+++ src/pose_viewer_server.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+app.mount('/static', StaticFiles(directory='./src/templates/static', html=True), name='static')
+templates = Jinja2Templates(directory='./src/templates')
 
 @app.get('/')
-async def get():
-  return HTMLResponse('hi')
+async def get(request: Request):
+  return templates.TemplateResponse(
+    name='index.html',
+    request=request,
+    context={'id': id}
+  )

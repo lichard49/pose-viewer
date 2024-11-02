b82ed529d67434d97c95aa1c1bc9d121c783a245
Richard Li <lichard49@gmail.com>
2024-11-01 22:25:03 -0700

    Imported three.js as a dependency

--- src/templates/static/script.js
+++ src/templates/static/script.js
@@ -1,1 +1,21 @@
-console.log('hi');
+import * as THREE from 'three';
+
+let camera;
+let scene;
+let renderer;
+
+function initViewer() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+
+  camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+  scene = new THREE.Scene();
+  scene.add(camera);
+
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(width, height);
+  renderer.render(scene, camera);
+  document.body.appendChild(renderer.domElement);
+}
+
+initViewer();

dab12e5c470d8a94c058e8984fa012174e9849e9
Richard Li <lichard49@gmail.com>
2024-12-26 15:40:17 -0800

    Implement new interface layout

---
+++ src/templates/static/output-cell.js
@@ -0,0 +1,69 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+class OutputCell extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const root = document.createElement('div');
+    root.setAttribute('class', 'output-cell');
+
+    const buttonRow = document.createElement('div');
+    const streamButton = document.createElement('button');
+    streamButton.setAttribute('class', 'output-cell-button');
+    streamButton.textContent = 'stream';
+    const uploadButton = document.createElement('button');
+    uploadButton.setAttribute('class', 'output-cell-button');
+    uploadButton.textContent = 'upload';
+    buttonRow.appendChild(streamButton);
+    buttonRow.appendChild(uploadButton);
+
+    root.appendChild(buttonRow);
+    this.initViewer()
+    this.renderer.domElement.setAttribute('class', 'output-cell-viewer');
+    root.appendChild(this.renderer.domElement);
+    this.append(root);
+  }
+
+  initViewer() {
+    const width = 575;
+    const height = 575;
+
+    // set up camera and scene
+    this.camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+    this.camera.position.z = 2.5;
+    this.scene = new THREE.Scene();
+    this.scene.add(this.camera);
+
+    // set up mesh
+    this.geometry = new THREE.BufferGeometry();
+    this.material = new THREE.MeshNormalMaterial();
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+
+    // set up renderer
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.setSize(width, height);
+    this.renderer.render(this.scene, this.camera);
+
+    // set up controller
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    controls.addEventListener('change', () => {
+      this.renderer.render(this.scene, this.camera);
+    });
+  }
+
+  renderPose(faces, vertices) {
+    const f = [...faces];
+    const v = new THREE.Float32BufferAttribute(vertices, 3);
+    this.geometry.setIndex(f);
+    this.geometry.setAttribute('position',  v);
+    this.geometry.computeVertexNormals();
+
+    this.renderer.render(this.scene, this.camera);
+  }
+}
+
+customElements.define('output-cell', OutputCell);
--- src/templates/static/script.js
+++ src/templates/static/script.js
@@ -1,72 +1,9 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+const centerPanel = document.getElementById('center-panel');
+const outputCells = centerPanel.getElementsByTagName('output-cell');
+const lastOutputCell = outputCells[outputCells.length - 1];
 
-let camera;
-let scene;
-let renderer;
-let geometry;
-let mesh;
-
-function initViewer(g) {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
-
-  // set up camera and scene
-  camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
-  camera.position.z = 2.5;
-  scene = new THREE.Scene();
-  scene.add(camera);
-
-  // set up mesh
-  geometry = g;
-  const material = new THREE.MeshNormalMaterial();
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
-
-  // set up renderer
-  renderer = new THREE.WebGLRenderer({antialias: true});
-  renderer.setSize(width, height);
-  renderer.render(scene, camera);
-  document.body.appendChild(renderer.domElement);
-
-  // set up controller
-  const controls = new OrbitControls(camera, renderer.domElement);
-  controls.addEventListener('change', () => {
-    renderer.render(scene, camera);
-  });
-}
-
-function renderPose(faces, vertices) {
-  const f = [...faces];
-  const v = new THREE.Float32BufferAttribute(vertices, 3);
-  geometry.setIndex(f);
-  geometry.setAttribute('position',  v);
-  geometry.computeVertexNormals();
-  geometry.rotateX(Math.PI);
-
-  renderer.render(scene, camera);
-}
-
-function renderBox(x, y, z) {
-  mesh.rotation.x = y;
-  mesh.rotation.y = x;
-  mesh.rotation.z = z;
-
-  renderer.render(scene, camera);
-}
-
-const modelLoader = document.getElementById('model-loader');
-
-function showModelLoader(show) {
-  if (show) {
-    modelLoader.style.display = 'block';
-  } else {
-    modelLoader.style.display = 'none';
-  }
-}
-
-let pose_faces = null;
-let pose_vertices = null;
+let poseFaces = null;
+let poseVertices = null;
 
 function setupWebsocket(mode, handler) {
   const origin = window.location.origin;
@@ -78,59 +15,18 @@ function setupWebsocket(mode, handler) {
   websocket.onmessage = handler;
 }
 
-const modeChooserPanel = document.getElementById('mode-chooser-panel');
-
-function showModeChooserPanel(show) {
-  if (show) {
-    modeChooserPanel.style.display = 'block';
-  } else {
-    modeChooserPanel.style.display = 'none';
-  }
-}
-
 function poseModeHandler(event) {
   if (event.data instanceof ArrayBuffer) {
-    if (pose_faces === null) {
-      pose_faces = new Uint32Array(event.data);
-    } else if (pose_vertices === null) {
-      pose_vertices = new Float32Array(event.data);
+    if (poseFaces === null) {
+      poseFaces = new Uint32Array(event.data);
+    } else if (poseVertices === null) {
+      poseVertices = new Float32Array(event.data);
 
-      showModelLoader(false);
-      renderPose(pose_faces, pose_vertices);
+      lastOutputCell.renderPose(poseFaces, poseVertices);
 
-      pose_vertices = null;
+      poseVertices = null;
     }
   }
 }
 
-function orientationModeHandler(event) {
-  if (event.data instanceof ArrayBuffer) {
-    const xyz = new Float32Array(event.data);
-    // console.log(xyz);
-
-    showModelLoader(false);
-    renderBox(xyz[0], xyz[1], xyz[2]);
-  }
-}
-
-const poseMode = document.getElementById('pose-mode');
-const orientationMode = document.getElementById('orientation-mode');
-
-function handleModeSelection(e) {
-  showModeChooserPanel(false);
-  showModelLoader(true);
-
-  switch(e.target) {
-    case poseMode:
-      initViewer(new THREE.BufferGeometry());
-      setupWebsocket('pose', poseModeHandler);
-      break;
-    case orientationMode:
-      initViewer(new THREE.BoxGeometry(3, 1, 3));
-      setupWebsocket('orientation', orientationModeHandler);
-      break;
-  }
-}
-
-poseMode.addEventListener('click', handleModeSelection);
-orientationMode.addEventListener('click', handleModeSelection);
+setupWebsocket('pose', poseModeHandler);
